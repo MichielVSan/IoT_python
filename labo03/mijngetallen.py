ff9a429664cfd51cd1f7d13ce91cc5d202eb030b
Michiel Van San <michielvansan@ap.be>
2021-10-06 15:22:33 +0200

    mijn getallen ingevoerd

---
+++ labo03/mijngetallen.py
@@ -0,0 +1,8 @@
+geboortejaar = 2003
+leeftijd = 18
+schoenmaat = 42
+print('naam getal', 'dec', 'hex', 'bin', sep='\t')
+print('-----------', '---', '---', '---', sep='\t')
+print('geboortejaar', geboortejaar, hex(geboortejaar), bin(geboortejaar), sep='\t')
+print('leeftijd', leeftijd, hex(leeftijd), bin(leeftijd), sep='\t')
+print('schoenmaat', schoenmaat, hex(schoenmaat), bin(schoenmaat), sep='\t')
