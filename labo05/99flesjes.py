4cf9c72040ef3e65acbfbabab4a9f06c897b0d8e
Michiel Van San <michielvansan@ap.be>
2021-10-20 14:56:31 +0200

    99 flesjes gemaakt

---
+++ labo05/99flesjes.py
@@ -0,0 +1,5 @@
+flesjesaantal=int(100)
+while flesjesaantal!=2:
+    flesjesaantal-=1
+    print(flesjesaantal, "flesjes met bier op de muur,", flesjesaantal, "flesjes met bier. Open er een, drink hem meteen.")
+print("1 fles met bier.....")
