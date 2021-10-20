7977f97a9559d03b318694035e8a844825fa73a9
Michiel Van San <michielvansan@ap.be>
2021-10-20 13:48:20 +0200

    punten/10 geven

--- labo05/maaltafels.py
+++ labo05/maaltafels.py
@@ -1,9 +1,12 @@
 # import the randint function from the module random:
 from random import randint
+puntentelling = 0
+right_answers = 0
 try:
     while True:
         n1 = randint(2, 10)
         n2 = randint(2, 10)
+        puntentelling +=1
         print('Wat is het product van volgende getallen?\n', n1, n2)
         try:
             answer = int(input('product: '))
@@ -14,7 +17,12 @@ try:
 
         if answer == product:
             print('Gefeliciteerd!')
+            right_answers+=1
         else:
             print('Het juiste antwoord was', product)
+        if puntentelling==10:
+            print(right_answers, "/10")
+            break
+
 except KeyboardInterrupt:
     print("tot de volgende")
